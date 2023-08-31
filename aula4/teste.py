2028913eb95069e5ef83c7a825ae6d6960ca2e0d
Ana <anaarantes@usp.br>
2023-08-31 12:00:00 -0300

    Script e resultado da aula 4.

---
+++ aula4/teste.py
@@ -0,0 +1,9 @@
+#Printando inputs do sistema com linha de comando e python
+import sys
+#sys.argv[0] se refere ao nome do arquivo
+valor1 = sys.argv[1]
+valor2 = sys.argv[2]
+valor3 = sys.argv[3]
+valor4 = sys.argv[4]
+
+print(f'Os valores são: {valor1}, {valor2}, {valor3}, {valor4}')
