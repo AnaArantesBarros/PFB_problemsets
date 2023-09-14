ab68f52d449baa8c3eaf8cd9676d604b0c767b31
Ana <anaarantes@usp.br>
2023-09-14 11:43:40 -0300

    Relatório 1

---
+++ Relatórios/Python1.py
@@ -0,0 +1,6 @@
+import sys
+print("Olá, Piracicaba!")
+nome = sys.argv[1]
+cor = sys.argv[2]
+acao = sys.argv[3]
+print(f'Olá,meu nome é {nome}! Minha cor favorita é {cor}. E minha atividade favorita é {acao]'}
